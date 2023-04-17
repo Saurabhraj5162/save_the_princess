8d04d5395bcb0e306048790841ba2ec7c3aad5cf
Alperen Erol <aerol2011@gmail.com>
2023-04-17 19:40:24 +0100

    final version of q and sarsa hp tuning

--- hpTuning.py
+++ hpTuning.py
@@ -9,7 +9,7 @@ time_steps = 2000
 alpha_lst = [0.4, 0.1]
 gamma_lst = [0.4, 0.1]
 temp_lst = [0.9]
-epsilon_lst = [0.9, 0.6, 0.4, 0.1]
+epsilon_lst = [0.9]
 epsilon_decay_lst = [0.999999]
 colors= ['r', 'b', 'g', 'y', 'c']
 Q = QLearning(6,6,600,600,(0,100,0))
@@ -28,37 +28,37 @@ temp_rewards = {}
 #     R_total = Q.run(10000,time_steps,epsilon=0.9, alpha=0.4, gamma=0.4, epsilon_decay = 0.999999)
 #     epsilon_decay_rewards[epsilon_decay] = R_total
 
-for temperature in temp_lst:
-    R_total = Q.run_softmax(10000,time_steps,temperature, alpha=0.4, gamma=0.4)
-    temp_rewards[temperature] = R_total
+# for temperature in temp_lst:
+#     R_total = Q.run_softmax(10000,time_steps,temperature, alpha=0.4, gamma=0.4)
+#     temp_rewards[temperature] = R_total
 
 
-for temperature, R_total in temp_rewards.items():
+# for temperature, R_total in temp_rewards.items():
 
-    Rtot_avg = np.array(R_total).cumsum() / np.arange(1, len(R_total) + 1)
-    plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha=0.4,gamma=0.4,temperature={temperature}')
-    plt.legend(loc="lower right")
-
-plt.xlabel('episodes')
-plt.ylabel('R')
-plt.title('Average total reward with softmax policy', fontsize=12, pad=20)
-plt.show()
-
-# for alpha in alpha_lst:
-#     for gamma in gamma_lst:
-#         R_total = Q.run(10000, time_steps, epsilon, alpha, gamma)
-#         alpha_gamma_rewards[(alpha, gamma)] = R_total
-
-# for alpha_gamma, R_total in alpha_gamma_rewards.items():
-#     alpha, gamma = alpha_gamma
-    
 #     Rtot_avg = np.array(R_total).cumsum() / np.arange(1, len(R_total) + 1)
-#     plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha={alpha},gamma={gamma},eps=0.9')
+#     plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha=0.4,gamma=0.4,temperature={temperature}')
 #     plt.legend(loc="lower right")
-    
+
 # plt.xlabel('episodes')
 # plt.ylabel('R')
-# plt.title('Average total reward with different alpha/gamma values', fontsize=12, pad=20)
+# plt.title('Average total reward with softmax policy', fontsize=12, pad=20)
 # plt.show()
+
+for alpha in alpha_lst:
+    for gamma in gamma_lst:
+        R_total = Q.run(10000, time_steps, 0.9, alpha, gamma, 0.999999)
+        alpha_gamma_rewards[(alpha, gamma)] = R_total
+
+for alpha_gamma, R_total in alpha_gamma_rewards.items():
+    alpha, gamma = alpha_gamma
+    
+    Rtot_avg = np.array(R_total).cumsum() / np.arange(1, len(R_total) + 1)
+    plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha={alpha},gamma={gamma},eps=0.9')
+    plt.legend(loc="lower right")
+    
+plt.xlabel('episodes')
+plt.ylabel('R')
+plt.title('Average total reward with different alpha/gamma values', fontsize=12, pad=20)
+plt.show()
 
 
--- hp_sarsa.py
+++ hp_sarsa.py
@@ -6,8 +6,8 @@ from sarsa_learning import SARSA
 
 epsilon = 0.9
 time_steps = 2000
-alpha_lst = [1, 0.9, 0.6, 0.4, 0.1]
-gamma_lst = [0.9, 0.7, 0.5, 0.3, 0.1]
+alpha_lst = [0.4]
+gamma_lst = [0.4]
 colors= ['r', 'b', 'g', 'y', 'c']
 Q = SARSA(6,6,600,600,(0,100,0))
 
@@ -24,11 +24,11 @@ plt.figure(figsize=(10,6))
 for i, alpha in enumerate(alpha_lst):
     R_total = alpha_rewards[alpha]
     Rtot_avg = np.array(R_total).cumsum() / np.arange(1, len(R_total) + 1)
-    plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha={alpha},gamma=0.9', color=colors[i])
+    plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha={alpha},gamma=0.4, epsilon={epsilon}', color=colors[i])
 plt.legend(loc="lower right")
 plt.xlabel('Episodes')
 plt.ylabel('Average total reward')
-plt.title('Average total reward with different alpha values', fontsize=12, pad=20)
+plt.title('Average total reward with SARSA algorithm', fontsize=12, pad=20)
 plt.show()
 
 # Loop over different gamma values
@@ -41,7 +41,7 @@ plt.figure(figsize=(10,6))
 for i, gamma in enumerate(gamma_lst):
     R_total = gamma_rewards[gamma]
     Rtot_avg = np.array(R_total).cumsum() / np.arange(1, len(R_total) + 1)
-    plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha=0.9,gamma={gamma}', color=colors[i])
+    plt.plot(np.arange(500, len(Rtot_avg)), Rtot_avg[500:], label=f'alpha=0.4,gamma={gamma}', color=colors[i])
 plt.legend(loc="lower right")
 plt.xlabel('Episodes')
 plt.ylabel('Average total reward')
